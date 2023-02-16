232e4bedcbec51a864d85664bfc41012e51f9ac0
pb-bennett <pb.bennett.uk@gmail.com>
2023-02-16 16:46:45 +0100

    Trip to the North

--- controllers/groupsController.js
+++ controllers/groupsController.js
@@ -1,12 +1,34 @@
 const db = require('../db');
 const initModels = require('../models/init-models');
+const { Op } = require('sequelize');
 
 const models = initModels(db);
 const searchBuilder = require('sequelize-search-builder');
 
 exports.getAllGroups = async function (req, res, next) {
   try {
-    const results = await models.invGroups.findAll();
+    let where = {};
+    if (req.query) {
+      // console.log(req.query);
+      const groupID = req.query.groupid.split(',').map(el => Number(el));
+      const start = new Date(req.query.start);
+      const end = new Date(req.query.end);
+      where = { groupID, start, end };
+      // console.log(groupIDs, start, end);
+    } else {
+      const data = await models.itemLookup.findAll();
+      const groupID = [...new Set(data.map(el => el.dataValues.groupID))];
+    }
+    console.log(where);
+    // console.log(groupIDs);
+    const results = await models.invGroups.findAll({
+      where: {
+        groupID: {
+          [Op.or]: groupIDs,
+        },
+      },
+      include: [{ all: true, nested: true, duplicating: false }],
+    });
 
     return res.status(200).json({
       status: 'success',
